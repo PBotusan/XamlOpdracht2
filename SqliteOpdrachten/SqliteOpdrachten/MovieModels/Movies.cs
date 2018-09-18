1c64c870ffbe2233a21db47fb71b441eb063235c
Paul Botusan <p_botusan@live.nl>
2018-09-18 13:19:03 +0200

    json/C# class aangepast

--- SqliteOpdrachten/SqliteOpdrachten/MovieModels/Movies.cs
+++ SqliteOpdrachten/SqliteOpdrachten/MovieModels/Movies.cs
@@ -7,60 +7,95 @@ namespace SqliteOpdrachten.MovieModels
 {
     class Movies
     {
-        
+
         public class Genre
         {
-            public int id { get; set; }
-            public string name { get; set; }
+            [JsonProperty("id")]
+            public int Id { get; set; }
+            [JsonProperty("name")]
+            public string Name { get; set; }
         }
 
         public class ProductionCompany
         {
-            public int id { get; set; }
-            public string logo_path { get; set; }
-            public string name { get; set; }
-            public string origin_country { get; set; }
+            [JsonProperty("id")]
+            public int Id { get; set; }
+            [JsonProperty("logo_path")]
+            public string LogoPath { get; set; }
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("orgin_country")]
+            public string OriginCountry { get; set; }
         }
 
         public class ProductionCountry
         {
-            public string iso_3166_1 { get; set; }
-            public string name { get; set; }
+            [JsonProperty("iso_3166_1")]
+            public string Iso31661 { get; set; }
+            [JsonProperty("name")]
+            public string Name { get; set; }
         }
 
         public class SpokenLanguage
         {
-            public string iso_639_1 { get; set; }
-            public string name { get; set; }
+            [JsonProperty("")]
+            public string Iso6391 { get; set; }
+            [JsonProperty("name")]
+            public string Name { get; set; }
         }
 
-        public class RootObject
+        public class Example
         {
-            public bool adult { get; set; }
-            public string backdrop_path { get; set; }
-            public object belongs_to_collection { get; set; }
-            public int budget { get; set; }
-            public List<Genre> genres { get; set; }
-            public string homepage { get; set; }
-            public int id { get; set; }
-            public string imdb_id { get; set; }
-            public string original_language { get; set; }
-            public string original_title { get; set; }
-            public string overview { get; set; }
-            public double popularity { get; set; }
-            public string poster_path { get; set; }
-            public List<ProductionCompany> production_companies { get; set; }
-            public List<ProductionCountry> production_countries { get; set; }
-            public string release_date { get; set; }
-            public int revenue { get; set; }
-            public int runtime { get; set; }
-            public List<SpokenLanguage> spoken_languages { get; set; }
-            public string status { get; set; }
-            public string tagline { get; set; }
-            public string title { get; set; }
-            public bool video { get; set; }
-            public double vote_average { get; set; }
-            public int vote_count { get; set; }
+            [JsonProperty("adult")]
+            public bool Adult { get; set; }
+            [JsonProperty("backdrop_path")]
+            public string BackdropPath { get; set; }
+            [JsonProperty("belongs_to_collection")]
+            public object BelongsToCollection { get; set; }
+            [JsonProperty("budget")]
+            public int Budget { get; set; }
+            [JsonProperty("genres")]
+            public IList<Genre> Genres { get; set; }
+            [JsonProperty("homepage")]
+            public string Homepage { get; set; }
+            [JsonProperty("id")]
+            public int Id { get; set; }
+            [JsonProperty("imdd_id")]
+            public string ImdbId { get; set; }
+            [JsonProperty("original_language")]
+            public string OriginalLanguage { get; set; }
+            [JsonProperty("original_title")]
+            public string OriginalTitle { get; set; }
+            [JsonProperty("overview")]
+            public string Overview { get; set; }
+            [JsonProperty("popularity")]
+            public double Popularity { get; set; }
+            [JsonProperty("poster_path")]
+            public string PosterPath { get; set; }
+            [JsonProperty("production_companies")]
+            public IList<ProductionCompany> ProductionCompanies { get; set; }
+            [JsonProperty("production_countries")]
+            public IList<ProductionCountry> ProductionCountries { get; set; }
+            [JsonProperty("release_date")]
+            public string ReleaseDate { get; set; }
+            [JsonProperty("revenue")]
+            public int Revenue { get; set; }
+            [JsonProperty("runtime")]
+            public int Runtime { get; set; }
+            [JsonProperty("spoken_languages")]
+            public IList<SpokenLanguage> SpokenLanguages { get; set; }
+            [JsonProperty("status")]
+            public string Status { get; set; }
+            [JsonProperty("tagline")]
+            public string Tagline { get; set; }
+            [JsonProperty("title")]
+            public string Title { get; set; }
+            [JsonProperty("video")]
+            public bool Video { get; set; }
+            [JsonProperty("vote_avarage")]
+            public double VoteAverage { get; set; }
+            [JsonProperty("vote_count")]
+            public int VoteCount { get; set; }
         }
     }
 }
